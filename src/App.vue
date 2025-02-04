288f8892ed62be8801569fbe98bd9bd2b5253ba9
Farah Fairuz <farahfairuz014@gmail.com>
2025-02-05 05:01:46 +0700

    style: update menubar styling

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import Navbar from '@/components/Navbar.vue';
+import Navbar from '@/components/MenuBar.vue';
 
 const savedTheme = localStorage.getItem('theme') as 'light' | 'dark' | null;
 const theme = ref<'light' | 'dark'>(savedTheme || 'light');
@@ -17,4 +17,7 @@ const toggleTheme = () => {
 </script>
 
 <style>
+body {
+  font-family: 'Helvetica', Arial, sans-serif;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import { createPinia } from 'pinia';
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/Home.vue';
 import MovieDetail from '@/pages/MovieDetail.vue';
 import Favorites from '@/pages/Favorites.vue';
 
-const savedTheme = localStorage.getItem('theme') as 'light' | 'dark' | null;
-const theme = savedTheme || 'light';
-
 const routes = [
   { path: '/', component: Home },
   { path: '/movie/:id', component: MovieDetail },
   { path: '/favorites', component: Favorites },
 ];
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
@@ -22,7 +21,11 @@ const router = createRouter({
 
 const app = createApp(App);
 
-app.use(PrimeVue, { theme });
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+});
 
 app.use(createPinia());
 app.use(router);
